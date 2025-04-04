bec849358311cfa85a396332d8e94d620889c4d1
Kahtaf Alam <kahtaf@gmail.com>
2025-04-04 00:15:04 -0400

    fix: ignore json file name

--- refiner/refine.py
+++ refiner/refine.py
@@ -25,34 +25,33 @@ class Refiner:
                 with open(input_file, 'r') as f:
                     input_data = json.load(f)
 
-                    if input_filename == 'user.json':
-                        # Transform account data
-                        transformer = UserTransformer(self.db_path)
-                        transformer.process(input_data)
-                        logging.info(f"Transformed {input_filename}")
+                    # Transform account data
+                    transformer = UserTransformer(self.db_path)
+                    transformer.process(input_data)
+                    logging.info(f"Transformed {input_filename}")
+                    
+                    # Create a schema based on the SQLAlchemy schema
+                    schema = OffChainSchema(
+                        name=settings.SCHEMA_NAME,
+                        version=settings.SCHEMA_VERSION,
+                        description=settings.SCHEMA_DESCRIPTION,
+                        dialect=settings.SCHEMA_DIALECT,
+                        schema=transformer.get_schema()
+                    )
+                    output.schema = schema
                         
-                        # Create a schema based on the SQLAlchemy schema
-                        schema = OffChainSchema(
-                            name=settings.SCHEMA_NAME,
-                            version=settings.SCHEMA_VERSION,
-                            description=settings.SCHEMA_DESCRIPTION,
-                            dialect=settings.SCHEMA_DIALECT,
-                            schema=transformer.get_schema()
-                        )
-                        output.schema = schema
-                            
-                        # Upload the schema to IPFS
-                        schema_file = os.path.join(settings.OUTPUT_DIR, 'schema.json')
-                        with open(schema_file, 'w') as f:
-                            json.dump(schema.model_dump(), f, indent=4)
-                            schema_ipfs_hash = upload_json_to_ipfs(schema.model_dump())
-                            logging.info(f"Schema uploaded to IPFS with hash: {schema_ipfs_hash}")
-                        
-                        # Encrypt and upload the database to IPFS
-                        encrypted_path = encrypt_file(settings.REFINEMENT_ENCRYPTION_KEY, self.db_path)
-                        ipfs_hash = upload_file_to_ipfs(encrypted_path)
-                        output.refinement_url = f"https://ipfs.vana.org/ipfs/{ipfs_hash}"
-                        continue
+                    # Upload the schema to IPFS
+                    schema_file = os.path.join(settings.OUTPUT_DIR, 'schema.json')
+                    with open(schema_file, 'w') as f:
+                        json.dump(schema.model_dump(), f, indent=4)
+                        schema_ipfs_hash = upload_json_to_ipfs(schema.model_dump())
+                        logging.info(f"Schema uploaded to IPFS with hash: {schema_ipfs_hash}")
+                    
+                    # Encrypt and upload the database to IPFS
+                    encrypted_path = encrypt_file(settings.REFINEMENT_ENCRYPTION_KEY, self.db_path)
+                    ipfs_hash = upload_file_to_ipfs(encrypted_path)
+                    output.refinement_url = f"https://ipfs.vana.org/ipfs/{ipfs_hash}"
+                    continue
 
         logging.info("Data transformation completed successfully")
         return output
