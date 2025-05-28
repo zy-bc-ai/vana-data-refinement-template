a4337ddb353698cbe60365b7fa84750a2539b4bf
zy-bc-ai <zy@xfair.ai>
2025-05-28 21:24:15 +0800

    Set up refiner for the DataDAO

--- refiner/config.py
+++ refiner/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     )
     
     SCHEMA_NAME: str = Field(
-        default="Google Drive Analytics",
+        default="Google Drive Analytics of FHEDataDAO",
         description="Name of the schema"
     )
     
